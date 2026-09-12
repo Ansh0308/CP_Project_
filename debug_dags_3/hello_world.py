e8632a8618e540ada4f0574751015687d7c3be1d
AddictedK1 <kaushalparmar018@gmail.com>
2026-09-12 12:53:27 +0530

    added some test dags to test failure

---
+++ debug_dags_3/hello_world.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "retries": 1,
+    "retry_delay": timedelta(seconds=30),
+}
+
+with DAG(
+    dag_id="hello_world",
+    description="Phase 2 sanity-check DAG: proves Airflow can parse and schedule DAGs",
+    default_args=default_args,
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["phase-2", "sanity-check"],
+) as dag:
+    say_hello = BashOperator(
+        task_id="say_hello",
+        bash_command="echo 'Hello from Airflow running in Docker Compose'",
+    )
